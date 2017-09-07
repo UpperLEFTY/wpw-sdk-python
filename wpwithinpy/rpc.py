65111fc233245a94ab46968a6a3c9d53432732e7
Berendt <wabe@MobicaPL.local>
2017-09-07 11:31:22 +0100

    WPW-34: rpc log file name - python impl

--- wpwithinpy/rpc.py
+++ wpwithinpy/rpc.py
@@ -1,6 +1,7 @@
 import launcher
 import os
 import logging
+import sys
 
 def startRPC(self, port, eventListenerPort):
   
@@ -9,13 +10,17 @@ def startRPC(self, port, eventListenerPort):
   reqArch = ["x64", "ia32"]
   cfg = launcher.Config(reqOS, reqArch);
   launcherLocal = launcher.launcher()
+  # define log file name for rpc agent, so e.g
+  # for "runConsumerOWP.py" it will be: "rpc-wpwithin-runConsumerOWP.log"
+  logfilename = os.path.basename(sys.argv[0])
+  logfilename = "rpc-wpwithin-" + logfilename.rsplit(".", 1)[0] + ".log"
   if eventListenerPort > 0:
       callbackPortString = " -callbackport=" + str(eventListenerPort)
   else:
   	  callbackPortString = ""
   
-  logging .debug(str(os.getcwd()) + "" + "-port " + str(port) + " -logfile wpwithin.log -loglevel debug,warn,error,fatal,info" + str(callbackPortString))
-  process = launcherLocal.launch(cfg, os.getcwd() + "", "-port " + str(port) + " -logfile wpwithin.log -loglevel debug,warn,error,fatal,info" + str(callbackPortString))
+  logging .debug(str(os.getcwd()) + "" + "-port " + str(port) + " -logfile " + logfilename + " -loglevel debug,warn,error,fatal,info" + str(callbackPortString))
+  process = launcherLocal.launch(cfg, os.getcwd() + "", "-port " + str(port) + " -logfile " + logfilename + " -loglevel debug,warn,error,fatal,info" + str(callbackPortString))
   
   return process
 
